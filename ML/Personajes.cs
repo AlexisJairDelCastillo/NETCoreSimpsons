c6ce15d1562d90e5fdcc7a8a30e3e201ba2a6360
AlexisJairDelCastillo <adelcastillocastillo1@gmail.com>
2023-11-17 15:50:40 -0600

    Add project files.

---
+++ ML/Personajes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Personajes
+    {
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public string Historia { get; set; }
+    }
+}
---
+++ PL/Controllers/SimpsonsAPIController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PL.Controllers
+{
+    public class SimpsonsAPIController : Controller
+    {
+        public ActionResult Simpsons()
+        {
+            return View();
+        }
+    }
+}
